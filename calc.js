4d72bb479619929ebc2e5a54f666f92382e249c5
Luis Guilherme <51214414+Becero@users.noreply.github.com>
2022-06-27 14:55:12 -0300

    atualizacao
    atualizei as funcoes

--- calc.js
+++ calc.js
@@ -2,15 +2,21 @@
 const calcular = document.getElementById('calcular');
 
 
-function imc(){
-    const nome = document.getElementById('nome');
-    const altura = document.getElementById('altura');
-    const peso = document.getElementById('peso');
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
-    if(nome.value !==''){
-        alert ('valor preenchido!')
-        resultado
+    if(nome !=='' && altura !=='' && peso !=='')
+    {
+        const valorIMC = (peso / (altura * altura));
+
+        resultado.textContent = valorIMC;
+
+    }
+        else{
+        resultado.textContent = 'Preencha todos os campos!!!'
     }
 }
 
